e15b2b569325e84d86f9a8d8bd1ac59d6f9b7ce8
zaghlol94 <zaghlollight@gmail.com>
2022-06-29 13:47:38 +0200

    update the model id in download_assets.sh

--- download_assets.sh
+++ download_assets.sh
@@ -1,7 +1,7 @@
 python -m spacy download de_core_news_sm
 python -m spacy download en_core_web_sm
-gdown "1p_AUVEtxG_4ZJLoG8w-pJ1UgKy47NB8P"
-mv seq2seq_phrase_rep.zip src/
+gdown "17ifILTP5iwnQHwX1e-2WVHFAuyTVnuqm"
+mv seq2seq_with_attention.zip src/
 cd src/
-unzip seq2seq_phrase_rep.zip
-rm seq2seq_phrase_rep.zip
+unzip seq2seq_with_attention.zip
+rm seq2seq_with_attention.zip
